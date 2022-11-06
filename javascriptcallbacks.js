70a5137c83037ae9fced4db93b54fc2e3071ea73
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2022-11-06 23:04:28 +0530

    javascript call backs exp 10

---
+++ javascriptcallbacks.js
@@ -0,0 +1,19 @@
+// Create an Array
+const myNumbers = [4, 1, -20, -7, 5, 9, -6];
+
+// Call removeNeg with a callback
+const posNumbers = removeNeg(myNumbers, (x) => x >= 0);
+
+// Display positive numbers
+document.getElementById("demo").innerHTML = posNumbers;
+
+// Keep only positive numbers
+function removeNeg(numbers, callback) {
+  const myArray = [];
+  for (const x of numbers) {
+    if (callback(x)) {
+      myArray.push(x);
+    }
+  }
+  return myArray;
+}
